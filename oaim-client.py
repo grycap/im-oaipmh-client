f6a0f162a4b6c2237ab161cdc742c4fdbea96d70
antonio <asanchezc30@gmail.com>
2025-04-03 15:22:52 +0200

    Updated client to accept endpoint as argument

--- oaim-client.py
+++ oaim-client.py
@@ -3,48 +3,37 @@ from lxml import etree
 from oaipmh_scythe import Scythe
 
 
-IM_ENDPOINT = 'https://deploy.sandbox.eosc-beyond.eu/im-oai/oai'
-
-
-def fetch_data_from_scythe(func, *args, **kwargs):
+def fetch_data_from_scythe(im_endpoint, func, *args, **kwargs):
     """Fetch data from Scythe."""
     try:
-        with Scythe(IM_ENDPOINT) as scythe:
+        with Scythe(im_endpoint) as scythe:
             return func(scythe, *args, **kwargs)
     except Exception as e:
         print(f"Repository response: {e}")
         return None
 
 
-def identify():
+def identify(im_endpoint):
     """Harvest repository identity information."""
-    identify_response = fetch_data_from_scythe(lambda scythe: scythe.identify())
+    identify_response = fetch_data_from_scythe(im_endpoint, lambda scythe: scythe.identify())
     if identify_response:
         print("Identify response:")
         print(etree.tostring(identify_response.xml, pretty_print=True, encoding="unicode"))
 
-def identify():
-    with Scythe(IM_ENDPOINT) as scythe:
-        try:
-            identify_response = scythe.identify()
-            print("Identify Response:")
-            print(etree.tostring(identify_response.xml, pretty_print=True, encoding="unicode"))
-        except Exception as e:
-            print("Error retrieving Identify response:", e)
 
-
-def list_metadata_formats(identifier=None):
+def list_metadata_formats(im_endpoint, identifier=None):
     """Harvest available metadata formats."""
-    metadata_formats = fetch_data_from_scythe(lambda scythe: scythe.list_metadata_formats(identifier))
+    metadata_formats = fetch_data_from_scythe(im_endpoint, lambda scythe: scythe.list_metadata_formats(identifier))
     if metadata_formats:
         print("\nMetadata formats:")
         for metadata_format in metadata_formats:
             print(etree.tostring(metadata_format.xml, pretty_print=True, encoding="unicode"))
 
 
-def list_identifiers(metadata_prefix, from_date=None, until=None, set_name=None):
+def list_identifiers(im_endpoint, metadata_prefix, from_date=None, until=None, set_name=None):
     """Harvest available identifiers."""
     identifiers = fetch_data_from_scythe(
+        im_endpoint,
         lambda scythe: scythe.list_identifiers(
             metadata_prefix=metadata_prefix,
             from_=from_date,
@@ -58,18 +47,19 @@ def list_identifiers(metadata_prefix, from_date=None, until=None, set_name=None)
             print(etree.tostring(identifier, pretty_print=True, encoding="unicode"))
 
 
-def list_sets():
+def list_sets(im_endpoint):
     """Harvest available sets."""
-    sets = fetch_data_from_scythe(lambda scythe: scythe.list_sets())
+    sets = fetch_data_from_scythe(im_endpoint, lambda scythe: scythe.list_sets())
     if sets:
         print("List sets:")
         for set_item in sets:
             print(etree.tostring(set_item, pretty_print=True, encoding="unicode"))
 
 
-def get_record(identifier, metadata_prefix):
+def get_record(im_endpoint, identifier, metadata_prefix):
     """Harvest a specific record."""
     record = fetch_data_from_scythe(
+        im_endpoint,
         lambda scythe: scythe.get_record(identifier=identifier, metadata_prefix=metadata_prefix)
     )
     if record:
@@ -77,9 +67,10 @@ def get_record(identifier, metadata_prefix):
         print(etree.tostring(record, pretty_print=True, encoding="unicode"))
 
 
-def list_records(metadata_prefix, from_date=None, until=None, set_name=None):
+def list_records(im_endpoint, metadata_prefix, from_date=None, until=None, set_name=None):
     """Harvest all records."""
     records = fetch_data_from_scythe(
+        im_endpoint,
         lambda scythe: scythe.list_records(
             from_=from_date,
             until=until,
@@ -95,9 +86,9 @@ def list_records(metadata_prefix, from_date=None, until=None, set_name=None):
 
 def get_arg_parser():
     parser = argparse.ArgumentParser(description='OAI-PMH client for Infrastructure Manager (https://im.egi.eu/im-dashboard/)')
+    parser.add_argument("--im-endpoint", required=True, help="Infrastructure Manager OAI-PMH endpoint")
     subparsers = parser.add_subparsers(dest='command')
 
-    # Identify arguments
     subparsers.add_parser('identify', help='Retrieve repository information')
 
     # List Identifiers arguments
@@ -133,17 +124,19 @@ if __name__ == '__main__':
     parser = get_arg_parser()
     args = parser.parse_args()
 
+    im_endpoint = args.im_endpoint
+
     if args.command == 'identify':
-        identify()
+        identify(im_endpoint)
     elif args.command == 'list_metadata_formats':
-        list_metadata_formats(identifier=args.identifier)
+        list_metadata_formats(im_endpoint, identifier=args.identifier)
     elif args.command == 'list_identifiers':
-        list_identifiers(metadata_prefix=args.metadata_prefix, from_date=args.from_date, until=args.until, set_name=args.set_name)
+        list_identifiers(im_endpoint, metadata_prefix=args.metadata_prefix, from_date=args.from_date, until=args.until, set_name=args.set_name)
     elif args.command == 'list_sets':
-        list_sets()
+        list_sets(im_endpoint)
     elif args.command == 'get_record':
-        get_record(identifier=args.identifier, metadata_prefix=args.metadata_prefix)
+        get_record(im_endpoint, identifier=args.identifier, metadata_prefix=args.metadata_prefix)
     elif args.command == 'list_records':
-        list_records(metadata_prefix=args.metadata_prefix, from_date=args.from_date, until=args.until, set_name=args.set_name)
+        list_records(im_endpoint, metadata_prefix=args.metadata_prefix, from_date=args.from_date, until=args.until, set_name=args.set_name)
     else:
         parser.print_help()
